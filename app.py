2d109096678b92eb12c2d68b7c17a5d7ea38716c
Kemal Beydilli <kembeydilli@gmail.com>
2023-05-20 01:19:33 +0300

    Post Register tamamlandı.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from endpoints import post_login
+from endpoints import post_register
 
 
 app = Flask(__name__)
@@ -12,3 +13,5 @@ def hello_world():
     }, 200
 
 app.register_blueprint(post_login.blueprint)
+app.register_blueprint(post_register.blueprint)
+
